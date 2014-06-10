43165aadc45371175160fb0f5596d7b13c3b24da
Scott Albertson <scott@thoughtbot.com>
2014-06-09 21:13:07 -0700

    Process markdown before saving email

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,9 +1,18 @@
 class Email < ActiveRecord::Base
   before_create :generate_slug
+  before_save :generate_html
 
   private
 
   def generate_slug
     self.slug = title.titleize.delete(" ").underscore.dasherize
+  end
+
+  def generate_html
+    self.html_body = markdown_renderer.render(content)
+  end
+
+  def markdown_renderer
+    @markdown_renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new)
   end
 end
